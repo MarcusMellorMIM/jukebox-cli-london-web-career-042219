b147cab79ff9d3278daa06f8587f16f026014890
Marcus Mellor <marcus@mellor.com>
2019-03-28 10:56:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,4 +11,17 @@ songs = [
 ]
 
 def help
-  
+  puts  "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "{- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list( songs )
+  songs.each_with_index do | i, v |
+    puts "#{i+1}. #{v}"
+  end
+end
+
+
